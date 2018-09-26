2a95741aae5358e6ff1363b5fda925067394e2d4
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-09-26 14:10:49 -0400

    Changes to our application
    Changes

---
+++ src/edu/osu/cse5234/controller/Order.java
@@ -0,0 +1,6 @@
+package edu.osu.cse5234.controller;
+
+public class Order {
+
+	public List<Item> = new ArrayList<>();
+}
---
+++ src/edu/osu/cse5234/controller/RequestMapping.java
@@ -0,0 +1,9 @@
+package edu.osu.cse5234.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+@org.springframework.web.bind.annotation.RequestMapping("/purchase")
+public class RequestMapping {
+
+}
