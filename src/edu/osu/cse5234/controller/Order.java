f8ef27ad96ac3163c7db445dfb93f0dba99ad47e
Naveen Tumkur Ramesh Babu <naveentrrko@gmail.com>
2018-09-26 14:27:54 -0400

    Models and JSP files
    Creation of models and JSP files for petCare project.

--- src/edu/osu/cse5234/controller/Order.java
+++
@@ -1,6 +0,0 @@
-package edu.osu.cse5234.controller;
-
-public class Order {
-
-	public List<Item> = new ArrayList<>();
-}
---
+++ src/edu/osu/cse5234/model/PaymentInfo.java
@@ -0,0 +1,33 @@
+package edu.osu.cse5234.model;
+
+public class PaymentInfo {
+
+	String credit_card_number;
+	String expiration_date;
+	String cvvCode;
+	String card_holder_name;
+	public String getCredit_card_number() {
+		return credit_card_number;
+	}
+	public void setCredit_card_number(String credit_card_number) {
+		this.credit_card_number = credit_card_number;
+	}
+	public String getExpiration_date() {
+		return expiration_date;
+	}
+	public void setExpiration_date(String expiration_date) {
+		this.expiration_date = expiration_date;
+	}
+	public String getCvvCode() {
+		return cvvCode;
+	}
+	public void setCvvCode(String cvvCode) {
+		this.cvvCode = cvvCode;
+	}
+	public String getCard_holder_name() {
+		return card_holder_name;
+	}
+	public void setCard_holder_name(String card_holder_name) {
+		this.card_holder_name = card_holder_name;
+	}
+}
